f969703dca5a60da6f6a22bfc8f3f503ba4ee79b
Divyaprabha M <dhivyaprabhamurugesan@gmail.com>
2020-03-29 20:46:27 +0530

    Update _ridge.py

--- sklearn/linear_model/_ridge.py
+++ sklearn/linear_model/_ridge.py
@@ -1618,13 +1618,13 @@ class _BaseRidgeCV(LinearModel):
 
         min_alpha = min(self.alphas)
         max_alpha = max(self.alphas) 
-        if (self.alpha_ in [min_alpha, max_alpha]):
-        warnings.warn("The optimal value for the regularization parameter "
-                      "'alpha' was {} \nwhich lies at a boundary of the "
-                      "explored range (between {} and {}). \nConsider "
-                      "setting the 'alphas' parameter to explore a "
-                      "wider range.".format(self.alpha_, min_alpha,
-                                        max_alpha))
+        if self.alpha_ in [min_alpha, max_alpha]:
+            warnings.warn("The optimal value for the regularization parameter "
+                          "'alpha' was {}\n which lies at a boundary of the "
+                          "explored range (between {} and {}).\n Consider "
+                          "setting the 'alphas' parameter to explore a "
+                          "wider range.".format(self.alpha_, min_alpha,
+                                            max_alpha))
 
         self.coef_ = estimator.coef_
         self.intercept_ = estimator.intercept_
